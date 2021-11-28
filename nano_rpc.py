dfa0c3d10404a8a4a6fc6247a5773b1e94f641de
gr0vity <gr0vity.dev@gmail.com>
2021-11-28 18:01:15 +0100

    initial commit : nano-local. Your own local nano env with configurable number of reps

---
+++ nano_rpc.py
@@ -0,0 +1,465 @@
+ 
+import requests
+import json
+import secrets
+import math
+import time
+import logging
+import os
+
+
+class Api:
+
+    # api_config = None
+    # debug = False
+   
+    def __init__(self, url):       
+        self.debug = True
+        self.RPC_URL = url
+    
+    def post_with_auth(self, content):
+        
+        url = self.RPC_URL 
+        headers = {"Content-type": "application/json", "Accept": "text/plain"}    
+        response = requests.post(url, json=content, headers=headers)
+        # print("request: {} \rrepsonse: {}".format(content["action"], response.text ))
+        return response
+ 
+
+    def generate_seed(self):
+        return secrets.token_hex(32)
+
+    def generate_new_seed(self):
+        return {'success': True,
+                'seed': self.generate_seed(),
+                'error_message': ''}
+
+    def validate_seed(self, seed):
+        result = {
+            'seed': seed,
+            'success': False,
+            'error_message': ''
+        }
+        if len(seed) == 64:
+            try:
+                int(seed, 16)
+                result['success'] = True
+
+            except Exception:
+                result['error_message'] = 'Wrong seed format'
+        else:
+            result['error_message'] = 'Wrong seed length'
+
+        return result
+    def get_account_data(self, seed):
+        payload = self.generate_account(seed, 0)
+        payload["success"] = True
+        payload["error_message"] = ''
+        
+        return payload
+
+    def get_account_data(self, seed, index):
+        payload = self.generate_account(seed, index)
+        payload["success"] = True
+        payload["error_message"] = ''
+        
+        return payload
+
+    def generate_account(self, seed, index):
+        
+        req_deterministic_key = {
+            "action": "deterministic_key",
+            "seed": seed,
+            "index": index,
+        }
+        r = self.post_with_auth(req_deterministic_key)
+        account_data = json.loads(r.text)
+        
+        account_data = {
+            "seed": seed,
+            "index": index,
+            "private": account_data["private"],
+            "public": account_data["public"],
+            "account": account_data["account"],
+            "nano_prefix": account_data["account"][0:11],
+            "nano_center": account_data["account"][11:59],
+            "nano_suffix": account_data["account"][len(account_data["account"]) - 6:]
+        }
+        return account_data
+
+    def validate_account_number(self, account):
+        response = {"success" : False}
+        req_validate_account_number = {
+            "action": "validate_account_number",
+            "account": account,
+        }
+        r = self.post_with_auth(req_validate_account_number)
+        data = json.loads(r.text)        
+        if data["valid"] == "1" :
+            response["success"] = True            
+        return response
+
+    def unlock_wallet(self, wallet, password):
+        response = {"success" : False}
+        req_password_enter = {
+        "action": "password_enter",
+        "wallet": wallet,
+        "password": password
+        }
+        r = self.post_with_auth(req_password_enter)
+        data = json.loads(r.text)        
+        if data["valid"] == "1" :
+            response["success"] = True            
+        return response
+
+    def wallet_create(self, seed):
+        # response = {"success" : False}
+        if seed == None :
+            req_wallet_create = {
+            "action": "wallet_create"
+        }
+        else :
+            req_wallet_create = {
+                "action": "wallet_create",
+                "seed": seed,
+            }
+        r = self.post_with_auth(req_wallet_create)
+        data = json.loads(r.text)        
+        # {
+        #     "wallet": "646FD8B5940AB5B1AD2C0B079576A4CF5A25E8ADB10C91D514547EF5C10C05B7",
+        #     "last_restored_account": "nano_3mcsrncubmquwcwgiouih17fjo8183t497c3q9w6qtnwz8bp3fig5x8m4rkw",
+        #     "restored_count": "1"
+        # }          
+        return data
+
+    def wallet_add(self, wallet, private_key) :
+        # response = {"success" : False}
+        req_wallet_add = {
+        "action": "wallet_add",
+        "wallet": wallet,
+        "key": private_key
+        }
+        r = self.post_with_auth(req_wallet_add)
+        data = json.loads(r.text)        
+        # {
+        #   "account": "nano_3e3j5tkog48pnny9dmfzj1r16pg8t1e76dz5tmac6iq689wyjfpi00000000"
+        # }        
+        return data
+        
+    def key_expand(self, private_key):        
+        req_key_expand = {
+            "action": "key_expand",
+            "key": private_key
+        }
+        r = self.post_with_auth(req_key_expand)
+        data = json.loads(r.text) 
+        return data           
+
+
+    def check_balance(self, account):
+       
+        multiplier = 10 ** 30
+        req_account_balance = {
+            "action": "account_balance",
+            "account": account,
+        }
+        r = self.post_with_auth(req_account_balance)
+        data = json.loads(r.text)
+       
+        return {"account": account, 
+                "balance_raw" : int(data["balance"]), 
+                "balance": self.truncate(int(data["balance"]) / multiplier), 
+                "pending": self.truncate(int(data["pending"]) / multiplier), 
+                "total": self.truncate((int(data["balance"]) + int(data["pending"])) / multiplier)}
+
+    def check_balances(self, seed):
+        # check if there is any balance for account 0 to 50 accounts
+        # {'index' : '' , 'account': '', 'balance': '', 'pending': ''}  ; spendable, total  Balance : 100 Nano . ! 95 Nano are currently not spendable. Your action is required.
+        result = []
+        for index in range(0, 51):
+            nano_account = self.generate_account(seed, index)
+            result.append(self.check_balance(nano_account["account"]))
+
+    def truncate(self, number):
+        if number > 0 :            
+            return str('{:8f}'.format(number))
+        else :
+            return "0.00"
+
+    def get_pending_blocks(
+        self,
+        nano_account,
+        threshold,
+        number_of_blocks
+    ) :
+
+        response = {"account" : nano_account,
+                    "blocks" : None,
+                    "success" : True,
+                    "error_message" : "" }
+
+        req_accounts_pending = {
+            "action": "accounts_pending",
+            "accounts": [nano_account],
+            "threshold" : str(threshold),
+            "sorting" : "true",
+            "count": str(number_of_blocks)
+        }       
+        r = self.post_with_auth(req_accounts_pending)
+        accounts_pending = json.loads(r.text)     
+
+        if "error" in accounts_pending:
+            response["success"] = False
+            response["error_message"] = accounts_pending["error"]
+        elif accounts_pending["blocks"][nano_account] == "" :        
+            response["success"] = False
+            response["error_message"] = "no pending blocks"
+        else :
+            response["blocks"] = accounts_pending["blocks"][nano_account]
+        
+        return response
+  
+    def create_open_block(
+        self,
+        destination_account,
+        open_private_key,
+        amount_per_chunk_raw,
+        rep_account,
+        send_block_hash,
+    ):
+
+        req_account_info = {
+            "action": "account_info",
+            "account": destination_account,
+            "representative": "true",
+            "pending": "true",
+            "include_confirmed": "true"
+        }
+        r = self.post_with_auth(req_account_info)
+        account_info = json.loads(r.text)
+
+        if "error" in account_info:
+            subtype = "open"
+            previous = "0000000000000000000000000000000000000000000000000000000000000000"
+            balance = str(amount_per_chunk_raw)
+        else:
+            subtype = "receive"
+            previous = account_info["frontier"]
+            balance = str(
+                int(account_info["confirmed_balance"]) + int(amount_per_chunk_raw))
+
+        # prepare open/receive block
+        req_block_create = {
+            "action": "block_create",
+            "json_block": "true",
+            "type": "state",
+            "balance": balance,
+            "key": open_private_key,
+            "representative": rep_account,
+            "link": send_block_hash,
+            "previous": previous
+            # ,"difficulty": difficulty,
+        }
+
+        r = self.post_with_auth(req_block_create)
+        block = json.loads(r.text)
+        next_hash = block["hash"]
+
+        req_process = {
+            "action": "process",
+            "json_block": "true",
+            "subtype": subtype,
+            "block": block["block"],
+        }
+
+        r = self.post_with_auth(req_process)
+        return {"success" : True,
+                "account" : destination_account, 
+                "balance_raw": balance,
+                "balance": self.truncate(int(balance) / (10 ** 30)), 
+                "hash": next_hash,
+                "amount_raw": amount_per_chunk_raw,
+                "amount": self.truncate(int(amount_per_chunk_raw) / (10 ** 30))}
+        # return next_hash
+
+    def create_send_block(
+        self,
+        source_seed,
+        source_index,
+        destination_account,
+        amount_per_chunk_raw
+    ):
+        if self.debug : t1 = time.time() 
+        req_source_account = {
+            "action": "deterministic_key",
+            "seed": source_seed,
+            "index": source_index,
+        }
+        r = self.post_with_auth(req_source_account)
+        if self.debug : logging.info("req_source_account : {}".format(time.time() - t1))
+        if self.debug : t1 = time.time() 
+
+        source_account_data = json.loads(r.text)
+        source_account_data = {
+            "seed": source_seed,
+            "index": source_index,
+            "private": source_account_data["private"],
+            "public": source_account_data["public"],
+            "account": source_account_data["account"],
+        }
+
+        req_account_info = {
+            "action": "account_info",
+            "account": source_account_data["account"],
+            "representative": "true",
+            "pending": "true",
+            "include_confirmed": "true"
+        }
+        r = self.post_with_auth(req_account_info)
+        account_info = json.loads(r.text)
+        if self.debug : logging.info("post_with_auth : {}".format(time.time() - t1))
+        if self.debug : t1 = time.time() 
+
+        source_previous = account_info["frontier"]
+        source_balance = account_info["balance"]
+        current_rep = account_info["representative"]
+
+        req_destination_key = {"action": "account_key",
+                               "account": destination_account}
+        r = self.post_with_auth(req_destination_key)
+        destination_link = json.loads(r.text)["key"]
+        if self.debug : logging.info("req_destination_key : {}".format(time.time() - t1))
+        if self.debug : t1 = time.time() 
+
+        # prepare send block
+        block_balance = str(int(source_balance) - int(amount_per_chunk_raw))
+        req_block_create = {
+            "action": "block_create",
+            "json_block": "true",
+            "type": "state",
+            "balance": str(block_balance),
+            "key": source_account_data["private"],
+            "representative": current_rep,
+            "link": destination_link,
+            "link_as_account": destination_account,
+            "previous": source_previous
+            # ,"difficulty": difficulty,
+        }
+
+        r = self.post_with_auth(req_block_create)
+        if self.debug : logging.info("req_block_create : {}".format(time.time() - t1))
+        if self.debug : t1 = time.time() 
+        send_block = json.loads(r.text)
+        logging.info(send_block["hash"])
+
+
+        req_process = {
+            "action": "process",
+            "json_block": "true",
+            "subtype": "send",
+            "block": send_block["block"],
+        }
+        r = self.post_with_auth(req_process)
+        if self.debug : logging.info("req_process : {}".format(time.time() - t1))
+        
+
+        # prepare for next iteration
+        #source_balance = block_balance
+        #source_previous = next_hash
+
+        # -------------- 2) END -------------------------
+        return {"success" : True,
+                "account" : source_account_data["account"], 
+                "balance_raw": block_balance,
+                "balance": self.truncate(int(block_balance) / (10 ** 30)), 
+                "hash": send_block["hash"],
+                "amount_raw": amount_per_chunk_raw,
+                "amount": self.truncate(int(amount_per_chunk_raw) / (10 ** 30))
+                }
+
+
+
+    def create_send_block_pkey(
+        self,
+        private_key,
+        source_account,
+        destination_account,
+        amount_per_chunk_raw
+    ):
+        if self.debug : t1 = time.time() 
+       
+        source_account_data = {    
+            "private": private_key,
+            "account": source_account,
+        }
+
+        req_account_info = {
+            "action": "account_info",
+            "account": source_account_data["account"],
+            "representative": "true",
+            "pending": "true",
+            "include_confirmed": "true"
+        }
+        r = self.post_with_auth(req_account_info)
+        account_info = json.loads(r.text)
+        if self.debug : logging.info("post_with_auth : {}".format(time.time() - t1))
+        if self.debug : t1 = time.time() 
+
+        source_previous = account_info["frontier"]
+        source_balance = account_info["balance"]
+        current_rep = account_info["representative"]
+
+        req_destination_key = {"action": "account_key",
+                               "account": destination_account}
+        r = self.post_with_auth(req_destination_key)
+        destination_link = json.loads(r.text)["key"]
+        if self.debug : logging.info("req_destination_key : {}".format(time.time() - t1))
+        if self.debug : t1 = time.time() 
+
+        # prepare send block
+        block_balance = str(int(source_balance) - int(amount_per_chunk_raw))
+        req_block_create = {
+            "action": "block_create",
+            "json_block": "true",
+            "type": "state",
+            "balance": str(block_balance),
+            "key": source_account_data["private"],
+            "representative": current_rep,
+            "link": destination_link,
+            "link_as_account": destination_account,
+            "previous": source_previous
+            # ,"difficulty": difficulty,
+        }
+
+        r = self.post_with_auth(req_block_create)
+        if self.debug : logging.info("req_block_create : {}".format(time.time() - t1))
+        if self.debug : t1 = time.time() 
+        send_block = json.loads(r.text)
+        logging.info(send_block["hash"])
+
+
+        req_process = {
+            "action": "process",
+            "json_block": "true",
+            "subtype": "send",
+            "block": send_block["block"],
+        }
+        r = self.post_with_auth(req_process)
+        if self.debug : logging.info("req_process : {}".format(time.time() - t1))
+        
+
+        # prepare for next iteration
+        #source_balance = block_balance
+        #source_previous = next_hash
+
+        # -------------- 2) END -------------------------
+        return {"success" : True,
+                "account" : source_account_data["account"], 
+                "balance_raw": block_balance,
+                "balance": self.truncate(int(block_balance) / (10 ** 30)), 
+                "hash": send_block["hash"],
+                "amount_raw": amount_per_chunk_raw,
+                "amount": self.truncate(int(amount_per_chunk_raw) / (10 ** 30))
+                }
+
+
---
+++ run_nano_local.py
@@ -0,0 +1,300 @@
+ 
+#!/usr/bin/env python
+"""
+Command-line tool using argparse
+"""
+import requests
+import json
+import logging
+import os
+import sys
+from nano_rpc import Api
+import argparse
+import math
+import time
+
+
+# GLOBAL variables
+url_1 = "http://localhost:45001"
+headers = {"Content-type": "application/json", "Accept": "text/plain"}
+path = "/root/testing/nanoBeta"
+
+
+def add_preconfigured_peers(preconfigured_peers, new_peer):
+    if preconfigured_peers == None :
+            preconfigured_peers = '"{}"'.format(new_peer)
+    else :
+        preconfigured_peers = '{},"{}"'.format(preconfigured_peers, new_peer)
+    return preconfigured_peers
+    
+
+def get_config_node_toml(preconfigured_peers, voting):
+    content= '''[node.websocket]
+# WebSocket server bind address.
+# type:string,ip
+address = "::ffff:0.0.0.0"
+# Enable or disable WebSocket server.
+# type:bool
+enable = true
+port = 47000
+
+[rpc]
+# Enable or disable RPC.
+# type:bool
+enable = true
+enable_sign_hash = true
+
+[node]
+work_threads = 1
+enable_voting = {voting}
+peering_port = 44000
+preconfigured_peers = {preconfigured_peers}'''.format(preconfigured_peers= preconfigured_peers, voting = voting)
+    return content
+     
+
+
+def get_config_rpc_toml():
+    content = '''
+# Bind address for the RPC server
+# type:string,ip
+address = "::ffff:0.0.0.0"
+
+# Enable or disable control-level requests
+# type:bool
+enable_control = true
+enable_sign_hash = true
+port = 45000
+
+[logging]
+log_rpc = false'''
+    return content
+
+
+def get_docker_compose_node_settings(i, pr_name):
+    content ='''  {pr_name}:\r
+    build: ./nano_local/nano-workspace/docker/.\r
+    user: "0"\r
+    command: ./nano-workspace/build/nano_node --daemon --config rpc.enable=true --network dev\r
+    container_name : {pr_name}\r
+    restart: unless-stopped\r
+    ports:\r
+    - 4400{i}:44000/udp\r
+    - 4400{i}:44000\r
+    - 4500{i}:45000\r
+    - 4700{i}:47000\r
+    volumes:\r
+    - ./reps/{pr_name}:/root\r
+    networks:\r
+    - nano-local\r\r'''.format(i=i, pr_name=pr_name)
+    return content
+    
+
+
+def make_pr(docker_conatiner, genesis_key = None):
+    # docker_conatiner="nano_local_pr1"
+    os.system("docker exec -it {} nano-workspace/build/nano_node --network dev --wallet_create".format(docker_conatiner))
+    wallet_pr=os.popen("docker exec -it {} nano-workspace/build/nano_node --network dev --wallet_list | awk 'FNR == 1 {{print $3}}' | tr -d '\r'".format(docker_conatiner)).read()
+    if genesis_key == None :
+       os.system("docker exec -it {} nano-workspace/build/nano_node --network dev --account_create --wallet={}".format(docker_conatiner,wallet_pr)) 
+    else:
+        os.system("docker exec -it {} nano-workspace/build/nano_node --network dev --wallet_add_adhoc --wallet={} --key=34F0A37AAD20F4A260F0A5B3CB3D7FB50673212263E58A380BC10474BB039CE4".format(docker_conatiner, wallet_pr)).read()
+    pr_address=os.popen("docker exec -it {} nano-workspace/build/nano_node --network dev --wallet_list | awk 'FNR == 2 {print $1}' | tr -d '\r')".format(docker_conatiner)).read()
+    return {"wallet" : wallet_pr, "nano_address" : pr_address}
+
+def make_pr_api(rpc_url, docker_conatiner, pr_type, genesis_key = None):
+    print(rpc_url , docker_conatiner, pr_type, genesis_key )
+    api = Api(rpc_url)
+    seed = api.generate_seed()
+    
+    account_data = {}
+    if genesis_key == None:
+        wallet_data = api.wallet_create(seed)
+        account_data = api.get_account_data(seed,0)
+        is_genesis = False
+    else :
+        wallet_data = api.wallet_create(None)
+        seed = None
+        account_data["account"] = api.wallet_add(wallet_data["wallet"], genesis_key)["account"]
+        account_data["private"] = genesis_key
+        is_genesis = True
+    
+    response = {"rpc_url" : rpc_url,
+                "docker_conatiner" : docker_conatiner,
+                "wallet" : wallet_data["wallet"] ,
+                "seed" : seed,               
+                "private_key" : account_data["private"],
+                "nano_address" : account_data["account"],
+                "is_genesis" : is_genesis ,
+                "pr_type": pr_type }   
+    return response
+    
+
+if __name__ == "__main__":
+
+    # Create the parser object, with its documentation message.
+    parser = argparse.ArgumentParser(description="Echo your input")
+
+    # Add a position-based command with its help message.    
+    parser.add_argument(
+        "--pr_quorum",
+        type=int,
+        help="#of prs to reach quorum",
+    )
+    parser.add_argument(
+        "--pr_non_quorum", type=int, help="#of prs who are not needed for quorum"
+    )
+
+    parser.add_argument(
+        "--build", type=bool, default=True, help="#of prs who are not needed for quorum"
+    )
+
+    parser.add_argument(
+        "--compose", type=bool, default=True, help="#of prs who are not needed for quorum"
+    )
+
+    # Use the parser to parse the arguments.
+    args = parser.parse_args()
+
+    print('''Default settings for the local nano network :
+    Your network will have {} major reps that hold 70 percent of the voting weight.
+    Your network will have {} minor reps that hold 0.015 percent of the voting weight each.
+    The remaining weight sits idle at the genesis account. This account will not vote.
+    When all your PRs are running, the network has {} percent online voting weight. 
+    '''.format(args.pr_quorum, pr_non_quorum, (70 + pr_non_quorum * 0.015)))
+    time.sleep(5)
+
+    #TODO : Min quroumPR = 1, max quroumPR = 900 , max totalPR = 990
+    os.system("mkdir -p nano_local")  
+    os.system("mkdir -p reps")  
+    os.system("mkdir -p output")  
+    if(os.path.isdir("nano_local/nano-workspace")) == False :
+        os.system("cd nano_local && git clone https://github.com/dsiganos/nano-workspace.git")  
+    if args.build :
+        os.system("cd nano_local/nano-workspace/docker && docker build -t gr0vity/local_beta:1.0 .")    
+    #TODO set flag when built succeeded. Dont run again!
+
+    total_pr = args.pr_quorum + args.pr_non_quorum + 1 #special case for genesis. make it as non voting
+    
+    #erase content of existing file
+    file1 = open(
+            "docker-compose.yml", "w", newline="\n"
+        )
+    file1.write("version: '3'\rservices:\r\r")
+    file1.close()
+    #append new content
+    file1 = open(
+            "docker-compose.yml", "a", newline="\n"
+        )
+
+    pr_names = {}
+    preconfigured_peers = None
+
+    for i in range(0, total_pr ): 
+        if i == 0 : 
+            pr_name  = "nano_local_genesis" 
+        else  :
+            pr_name = "nano_local_pr{}".format(i)
+        preconfigured_peers = add_preconfigured_peers(preconfigured_peers,pr_name)
+        os.system("cd reps && mkdir -p {}".format(pr_name))
+        if i <= args.pr_quorum : 
+            if i == 0 : #genesis
+                pr_names[pr_name] = {"pr_type" : "genesis", 
+                                     "genesis_key" : "34F0A37AAD20F4A260F0A5B3CB3D7FB50673212263E58A380BC10474BB039CE4", 
+                                     "peering_port": "4400{}".format(i), 
+                                     "rpc_port" : "4500{}".format(i)}
+            else:
+                pr_names[pr_name] = {"pr_type" : "pr_quorum", 
+                                     "genesis_key" : None, 
+                                     "peering_port": "4400{}".format(i),
+                                     "rpc_port" : "4500{}".format(i)}            
+        else :
+            pr_names[pr_name] = {"pr_type" : "pr_non_quorum", "genesis_key" : None, "peering_port": "4400{}".format(i), "rpc_port" : "4500{}".format(i)}
+  
+        
+        file1.write(get_docker_compose_node_settings(i, pr_name))
+    file1.write("\rnetworks:\r")
+    file1.write("  nano-local:\r")
+    file1.write("    driver: bridge\r")
+    file1.close()
+
+    preconfigured_peers = "[{}]".format(preconfigured_peers)
+    print(preconfigured_peers)
+
+      
+    for key, value in pr_names.items(): 
+        os.system("cd reps/{} && mkdir -p NanoDev".format(key))  
+        voting = "false" if key == "genesis" else "true" 
+        f_config_node = open(
+            "reps/{}/NanoDev/config-node.toml".format(key), "w", newline="\n"
+        )
+        f_config_node.write(get_config_node_toml(preconfigured_peers,voting))
+        f_config_node.close()
+
+        f_config_rpc = open(
+            "reps/{}/NanoDev/config-rpc.toml".format(key), "w", newline="\n"
+        )
+        f_config_rpc.write(get_config_rpc_toml())
+        f_config_rpc.close()
+
+    exit
+
+    if args.compose :
+    #docker compose bugs. works eventually when trying once for each PR
+        # for i in range(1, total_pr + 1):
+        #     time.sleep(2)  
+        #     os.system("docker-compose up -d") 
+        os.system("docker-compose up -d")    
+
+    #API calls to create wallets. account and seed data for PRs
+    pr_data = {}
+    f = open(
+        "output/pr_data.txt", "w", newline="\n"
+    )
+    f.write("PR seeds wallets and more\r")
+    f.close()
+    for key, value in pr_names.items():        
+        result = make_pr_api("http://127.0.0.1:{}".format(value["rpc_port"]),
+                                    key, 
+                                    value["pr_type"],
+                                    value["genesis_key"] )
+                         
+        f = open(
+            "output/pr_data.txt", "a", newline="\n"
+        )
+        f.write("{}\r".format(str(result)))
+        f.close()
+        pr_data[key] = result
+
+   
+
+    for key, genesis in pr_data.items():        
+        if genesis["pr_type"] == "genesis" : 
+            api = Api(genesis["rpc_url"])  
+            genesis_balance = int(api.check_balance(genesis["nano_address"])["balance_raw"])
+            
+            for key, pr in pr_data.items():
+                if pr["pr_type"] == "pr_quorum" :
+                    pr_balance_raw = round((genesis_balance * 0.7) /  args.pr_quorum)
+                elif pr["pr_type"] == "pr_non_quorum" :
+                    pr_balance_raw = round(genesis_balance * 0.015)
+                else:
+                    print(str(pr))
+                    continue
+                
+                send_block = api.create_send_block_pkey(genesis["private_key"],
+                                        genesis["nano_address"],
+                                        pr["nano_address"],
+                                        pr_balance_raw)
+                print("SEND FROM {} To {} : HASH {}".format(genesis["nano_address"],pr["nano_address"],send_block["hash"] ))
+                open_block = api.create_open_block(pr["nano_address"],
+                                    pr["private_key"],
+                                    pr_balance_raw,
+                                    pr["nano_address"],
+                                    send_block["hash"]
+                                    )
+                print("OPENED for {} with PR {}".format(pr["nano_address"],pr["nano_address"] ))                
+                    
+
+        
+    
+
