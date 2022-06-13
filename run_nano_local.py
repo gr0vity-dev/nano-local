581193fd548c3afb3878b14e1db3c4b87406aca4
gr0vity-dev <iq.cc@pm.me>
2022-06-13 22:14:38 +0200

    add restart_wait_sync command.

--- run_nano_local.py
+++ run_nano_local.py
@@ -11,6 +11,7 @@ from src.nano_local_initial_blocks import InitialBlocks
 from src.nano_rpc import NanoRpc
 import argparse
 import copy
+import time
 
 # * create (this will create the one time resources that need creating)
 # * start (this will start the nodes)
@@ -125,8 +126,7 @@ def write_docker_compose_env(compose_version):
 
 def subprocess_read_lines(command):
     try:
-        res = check_output(command, shell=True, stderr=STDOUT, encoding='UTF-8')       
-        print(len(res))
+        res = check_output(command, shell=True, stderr=STDOUT, encoding='UTF-8')        
     except CalledProcessError as e:
         raise RuntimeError(f"command '{e.cmd}' return with error (code {e.returncode}): {e.output}")  
     return res.splitlines()
@@ -138,9 +138,8 @@ def generate_genesis_open(genesis_key):
         docker_stop_rm = """docker stop ln_get_genesis 1>/dev/null &&
                             docker rm ln_get_genesis 1>/dev/null &"""
 
-
-        logging.info("run temporary docker conatiner for genesis generation")       
-        blocks = ''.join(subprocess_read_lines(docker_exec)[104:112])   
+        logging.info("run temporary docker conatiner for genesis generation")         
+        blocks = ''.join(subprocess_read_lines(docker_exec)[104:112]) 
         logging.info("stop and remove docker container")        
         call(docker_stop_rm, shell=True)
         return json.loads(str(blocks))
@@ -197,6 +196,7 @@ def create_nodes(compose_version, genesis_node_name = "nl_genesis"):
     prepare_nodes(genesis_node_name = genesis_node_name)
     write_docker_compose_env(compose_version)
     _conf.write_docker_compose()
+    _conf.print_enabled_services()
 
 def start_all(build_f):
     dir_nano_nodes = _node_path["container"]
@@ -204,8 +204,7 @@ def start_all(build_f):
     if build_f :
         command =  f'cd {dir_nano_nodes} && docker-compose up -d --build'
     system(command)
-    is_rpc_available(_conf.get_nodes_name())
-                 
+    is_rpc_available(_conf.get_nodes_name())                
                  
 
 def start_nodes():
@@ -230,6 +229,21 @@ def stop_nodes():
 def restart_nodes():
     stop_nodes()
     start_nodes()
+
+def restart_wait_sync():
+    from src.nano_block_ops import BlockAsserts
+    ba = BlockAsserts()
+    all_cemented = False
+    while not all_cemented:
+        try :
+            block_count = ba.assert_all_blocks_cemented()
+            all_cemented = True
+        except:
+            logging.info("Not all blocks cemented... restarting nodes.")
+            restart_nodes()
+            time.sleep(10)
+    logging.getLogger().success(f'All {block_count["cemented"]} blocks are cemented')
+    
 
 def reset_nodes():
     stop_nodes()
@@ -328,7 +342,7 @@ def main():
 
     elif args.command == 'init':
         init_nodes()
-        restart_nodes()
+        #restart_nodes()
         logging.getLogger().success("ledger initialized")
 
     elif args.command == 'stop':
@@ -342,6 +356,9 @@ def main():
     elif args.command == 'restart':
         restart_nodes()
         logging.getLogger().success("nodes restarted")
+    
+    elif args.command == 'restart_wait_sync':
+        restart_wait_sync()        
 
     elif args.command == 'reset':
         reset_nodes()
